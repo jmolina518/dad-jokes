3bfa9f2b4e597d744531e0f96097809597752bf2
jmolina518 <jmolina518@gmail.com>
2021-02-25 22:56:02 -0600

    add async/await function

--- script.js
+++ script.js
@@ -5,18 +5,33 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-// USING .then()
-
-function generateJoke() {
+// Using ASYNC/AWAIT
+async function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
     },
   }
 
-  fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke
-    })
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
 }
+
+// USING .then()
+
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
